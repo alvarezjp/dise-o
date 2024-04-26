8fd614e540274052b1c846b61033ee5079be9616
Jesus <6.jesus.a@gmail.com>
2024-04-25 21:27:58 -0400

    verificacionMovil

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import "./style.css"
 import Text from "./Text"
 import CoffeIcon from './CoffeIcon';
+import { useEffect } from 'react';
 
 function Home() {
+    useEffect(() => {
+        // Código para aplicar estilos dinámicos aquí
+        const altura = window.innerHeight;
+        const calculo = altura * 0.9;
+        const elemento = document.getElementById("esto");
+        elemento.style.height = `${calculo}px`
+    }, []);
     return (
-        <section className='home'>
+        <section className='home' id='esto'>
             <Text/>
             <CoffeIcon/>
         </section>
--- src/components/home/Text.jsx
+++ src/components/home/Text.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Text() {
+   const altura = window.innerHeight;
+   const calculo = altura * 0.5;
+
   return (
     <article className="article">
-      <h2 className="homeText">Creando experiencias digitales</h2>
+      <h2 className="homeText">Creando experiencias digitales , {altura},{calculo}</h2>
     </article>
   );
 }
