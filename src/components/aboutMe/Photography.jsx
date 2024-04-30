11a889544fcbac264634f95832100120fa13fe53
Jesus <6.jesus.a@gmail.com>
2024-04-29 22:23:02 -0400

    verificacionMovil

--- src/components/aboutMe/Photography.jsx
+++ src/components/aboutMe/Photography.jsx
@@ -4,11 +4,6 @@ const Photography = () => {
   return (
     <article className="photoSection">
       <img
-        src="/img/background.svg"
-        alt="Fondo vende"
-        className="backgroundPhoto"
-      />
-      <img
         src="/img/photography.svg"
         alt="Fotografia "
         className="photography"
