6d18d6deecdc5d80232ad7a36f10c833aed307ad
Jesus <6.jesus.a@gmail.com>
2024-05-09 06:13:59 -0400

    verificacionMovil

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,7 +4,7 @@ import "./style.css"
 const Skills = () => {
     return (
         <section className='skills'>
-            
+            <img src="/icons/Frame 11.png" alt="" />
         </section>
     );
 };
