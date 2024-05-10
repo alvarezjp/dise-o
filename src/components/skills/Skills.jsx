ecabb67b33ba75d776585e272ecb61f3b6a93cc1
Jesus <6.jesus.a@gmail.com>
2024-05-09 23:24:08 -0400

    verificacionMovil

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import "./style.css";
 
 const Skills = () => {
-  const arrayIcons = Array.from({ length: 10 }, (_, index) => index + 1);
+  const arrayIcons = Array.from({ length: 9 }, (_, index) => index + 1);
   return (
     <section className="skills">
-      {arrayIcons.map((item,index) => {
-        return (
-          <>
-            {/* <img key={item} src={`./icons/skills${item}.png`} alt="" />` */}
-          </>
-        );
-      })}
+      <article className="contain">
+        {arrayIcons.map((item, index) => {
+          return (
+            <img
+              key={index}
+              src={`./icons/skills${item}.png`}
+              alt="Iconos de skills"
+              className="iconSkills"
+            />
+          );
+        })}
+      </article>
     </section>
   );
 };
