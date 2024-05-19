9612bd7e04ff146b780ecdb3573a3cdb44b18911
Jesus <6.jesus.a@gmail.com>
2024-05-19 12:33:46 -0400

    verificacionMovil

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,39 +1,62 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useRef, useEffect } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SkillsContainer = styled.section`
+  padding: 0px 21px;
+  box-sizing: border-box;
+  height: 180px;
+  overflow: hidden;
+  align-content: center;
+  background-color: var(--color2);
+  /* border: 3px solid black; */
+`;
+
+const ContainArticle = styled.div`
+  
+  height: auto;
+  box-sizing: border-box;
+ 
+  /* border: 3px solid red; */
+`;
+const IconSkills = styled.img`
+  width: 96px;
+  height: 96px;
+  /* border: 3px solid blue; */
+`;
 
 const Skills = () => {
-  const mouseDown = () => {
-    console.log("click down");
-  };
-
-  const mouseUp = () => {
-    console.log("click up");
-  };
-
-  const mouseMove = () => {
-    console.log("Mouse Move");
-  };
-
   const arrayIcons = Array.from({ length: 9 }, (_, index) => index + 1);
+
+  const settings = {
+    dots: false,
+    arrows:false,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 3, // Ajusta según la cantidad de íconos visibles al mismo tiempo
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 1000, // Ajusta la velocidad de autoplay
+    pauseOnHover: true,
+  };
+
   return (
-    <section className="skills">
-      <article
-        className="contain"
-        onMouseDown={mouseDown}
-        onMouseUp={mouseUp}
-        onMouseMove={mouseMove}>
-        {arrayIcons.map((item, index) => {
-          return (
-            <img
+    <SkillsContainer>
+      <Slider {...settings}>
+        {arrayIcons.map((item, index) => (
+          <ContainArticle>
+            <IconSkills
               key={index}
               src={`./icons/skills${item}.png`}
               alt="Iconos de skills"
               className="iconSkills"
             />
-          );
-        })}
-      </article>
-    </section>
+          </ContainArticle>
+        ))}
+      </Slider>
+    </SkillsContainer>
   );
 };
 
