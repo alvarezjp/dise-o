3947ca6fe46ed4d447f0816c9a2a867e017d5d98
Jesus <6.jesus.a@gmail.com>
2024-05-31 00:58:38 -0400

    verificacionMovil

--- src/components/contactMe/form/Form.jsx
+++ src/components/contactMe/form/Form.jsx
@@ -1,24 +1,59 @@
 import React from "react";
-import "./styleForm.css"
+import "./styleForm.css";
+import { useState } from "react";
 
 const Form = () => {
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    email: "",
+    comment: "",
+  });
+  const formSubmit = (event) => {
+    event.preventDefault();
+    console.log("se detuvo del envio");
+  };
   return (
-    <article className="formContact">
-        <h2 className="formTitle">Formulario de contacto</h2>
+    <form className="formContact" autocomplete="off" onSubmit={formSubmit}>
+      <h2 className="formTitle">Formulario de contacto</h2>
       <div className="boxInput">
-        <label htmlFor="name" className="labelForm">Nombre</label>
-        <input type="text" name="name" placeholder="Nombre"  className="formInput"/>
+        <label htmlFor="name" className="labelForm">
+          Nombre
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={inputValue.name}
+          placeholder="Nombre"
+          className="formInput"
+          required
+        />
       </div>
       <div className="boxInput">
-        <label htmlFor="email" className="labelForm">Correo electrónico</label>
-        <input type="email" name="email" placeholder="correo@mail.com" className="formInput" />
+        <label htmlFor="email" className="labelForm">
+          Correo electrónico
+        </label>
+        <input
+          type="email"
+          name="email"
+          placeholder="correo@mail.com"
+          className="formInput"
+          required
+        />
       </div>
       <div className="boxInput">
-        <label htmlFor="comment" className="labelForm">Comentario</label>
-        <textarea name="comment"placeholder="Escribe aqui..." className="formInput formTextArea"></textarea>
+        <label htmlFor="comment" className="labelForm">
+          Comentario
+        </label>
+        <textarea
+          name="comment"
+          placeholder="Escribe aqui..."
+          className="formInput formTextArea"
+          required></textarea>
       </div>
-      <button type="submit" className="formButton">ENVIAR</button>
-    </article>
+      <button type="submit" className="formButton">
+        ENVIAR
+      </button>
+    </form>
   );
 };
 
