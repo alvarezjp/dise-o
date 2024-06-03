340313e801fc76b0c2ea66ce67d2863bbc52ecc2
Jesus <6.jesus.a@gmail.com>
2024-06-02 21:05:18 -0400

    verificacionMovil

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <section>
-        <img src="./img/logo.svg" alt="Logotipo" className="logoFooter"/>
+        <a href="#"> <img src="./img/logo.svg" alt="Logotipo" className="logoFooter"/></a>
       </section>
       <section className="sectionName">
         <p className="textFooter">© 2022-2024 Jesús Álvarez.</p>
@@ -15,7 +15,7 @@ const Footer = () => {
       </section>
       <section className="sectionDesigner"> 
         <p className="textFooter">Diseñado por </p>
-        <p className="designer" onClick={urlBehance}>Francisca Pérez.</p>
+        <p className="designer" onClick={urlBehance}>Fran.dvisual</p>
       </section>
     </footer>
   );
