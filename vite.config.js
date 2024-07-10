14e72340af5d7616b4a42473cf1936cc5dac4a18
Jesus <6.jesus.a@gmail.com>
2024-07-09 23:09:17 -0400

    agregando modificacional form

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
